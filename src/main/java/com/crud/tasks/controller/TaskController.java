d19202d31ad1e5b7512f6413958344b376a5d49d
bujak.rafal@yahoo.com <bujak.rafal@yahoo.com>
2018-05-23 18:26:29 +0200

    adding annotation for frondend aplication

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/task")
 public class TaskController {
 
