1fe99687fa9011622968a8d1348d14c0b22acdc5
bujak.rafal@yahoo.com <bujak.rafal@yahoo.com>
2018-06-18 22:52:56 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
 @Configuration
 public class CoreConfiguration {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    public RestTemplate restTemplate() { return new RestTemplate(); }
+
+
 }
