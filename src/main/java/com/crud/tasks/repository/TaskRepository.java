acd0a44084359137b4cadea803ac01393a72e06f
Rafal Bujak <bujak.rafal@yahoo.com>
2018-06-20 23:43:41 +0200

    adding scheduled annotation

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -13,6 +13,8 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     Task save(Task task);
 
+    @Override
+    long count();
 
     Optional<Task> findById(Long id);
 }
